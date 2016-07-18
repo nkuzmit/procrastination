7846f047146ce9395be4a5fd819b348df084bf02
nkuzmit <n.kuzmit@gmail.com>
2016-07-19 02:02:55 +0300

    pfff

--- hw1/Compare102.java
+++ hw1/Compare102.java
@@ -6,17 +6,16 @@ public class Compare102 {
 	
 	public static void main(String[] args){
 		
-		System.setProperty("console.encoding","cp866");	
-		System.out.print("Введи x\n");
+		System.setProperty("console.encoding","cp866");	//На всякий случай задаю кодировку
+		System.out.print("Введи x\n");					//Прошу ввести первое из сравниваемых чисел
 		Scanner first = new Scanner(System.in); 
 		int x = first.nextInt();
 		
-		System.out.print("Введи y\n");
+		System.out.print("Введи y\n");					//Прошу ввести второе число
 		Scanner second = new Scanner(System.in); 
 		int y = second.nextInt();
 		
-		System.setProperty("console.encoding","cp866");		
-		if(x < y) System.out.println("x меньше y");
+		if(x < y) System.out.println("x меньше y");		//Сам процесс сравнения и вывод результата
 		
 		if(x == y) System.out.println("x равен y");
 		
