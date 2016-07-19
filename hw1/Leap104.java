b94d360a1b5312ef209a33cf190c6a01d1565f91
nkuzmit <n.kuzmit@gmail.com>
2016-07-19 21:31:55 +0300

    Забыл еще одну проверку

--- hw1/Leap104.java
+++ hw1/Leap104.java
@@ -10,11 +10,11 @@ public static void main(String[] args){
 		Scanner first = new Scanner(System.in); 
 		int year = first.nextInt();
 		
-		if (year % 4 != 0){
+		if (year % 4 != 0){								//Проверяю делением на 4, возможно сразу определю не високосный
 			System.out.println("Этот год не високосный!");
 		}
 		else {
-			if(year % 400 == 0){
+			if(year % 100 == 0 && year % 400 ==0){						//Если дошел сюда, проверяю делением на 100 и 400
 				System.out.println("Этот год високосный!");
 			}
 		}
