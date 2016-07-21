1e9064f120cc787c97eeed2ccd1007b5198c7c46
nkuzmit <n.kuzmit@gmail.com>
2016-07-21 18:03:25 +0300

    2 домашка 3*

---
+++ hw1/SumFraction.java
@@ -0,0 +1,26 @@
+package hw1;
+
+import java.util.Scanner;
+
+public class SumFraction {
+
+public static void main(String[] args){
+		
+		System.setProperty("console.encoding","cp866");	//На всякий случай задаю кодировку
+		System.out.print("Задайте n:\n");	//Принимаем n
+		Scanner num = new Scanner(System.in); 
+		int n = num.nextInt();
+		
+		float f = 0;
+		
+		for(float i = 2; i <= n; i++){
+			f = (1 / i)+ f;
+			
+			//System.out.println(f);
+		}
+		System.out.println(1 + f);
+		
+		num.close();	
+}
+
+}
