0f7e9245b35b323670b2b059d8987071e19c2eae
nkuzmit <n.kuzmit@gmail.com>
2016-07-23 14:20:09 +0300

    ,

--- src/hw0/Hello.java
+++ src/hw0/Hello.java
@@ -2,6 +2,6 @@ package hw0;
 
 public class Hello {
 	public static void main(String[] args) {		
-		System.out.println("Привет, Java!"); //Начинаю сначала. Simple basic stuff. da
+		System.out.println("Привет, Java!"); //Начинаю сначала. Simple basic stuff.
 		} 
 	}
