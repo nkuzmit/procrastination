786a8eba779e79944d6e02b61dc750779c354a82
nkuzmit <n.kuzmit@gmail.com>
2016-07-23 18:59:58 +0300

    this drives me cray

--- src/hw0/PrivetUsername.java
+++ src/hw0/PrivetUsername.java
@@ -6,11 +6,11 @@ public class PrivetUsername {
 	
 	public static void main(String[] args) {
 	
-		System.out.print("Как тебя зовут?\n");
-		Scanner greeting = new Scanner(System.in, "cp866"); 
-		System.setProperty("console.encoding","cp866");//Должно выводить на русском, вроде
+		System.out.print("РљР°Рє С‚РµР±СЏ Р·РѕРІСѓС‚?\n");
+		Scanner greeting = new Scanner(System.in);
 		String userName = greeting.next();
-		System.out.print("Привет, " + userName + "!");
+		System.out.print("РџСЂРёРІРµС‚, " + userName + "!");
 		greeting.close();
 	}
+	
 }
