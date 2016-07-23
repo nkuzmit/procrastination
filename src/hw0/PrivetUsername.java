9cfc0c3caa1ead104eaa707b2c055d3d199640b3
nkuzmit <n.kuzmit@gmail.com>
2016-07-24 01:02:21 +0300

    повторение

--- src/hw0/PrivetUsername.java
+++ src/hw0/PrivetUsername.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class PrivetUsername {
 	
 	public static void main(String[] args) {
-	
+
 		System.out.print("Как тебя зовут?\n");
-		Scanner greeting = new Scanner(System.in);
+		Scanner greeting = new Scanner(System.in, "cp866"); 
+		System.setProperty("console.encoding","cp866");//Должно выводить на русском, вроде
 		String userName = greeting.next();
 		System.out.print("Привет, " + userName + "!");
 		greeting.close();
