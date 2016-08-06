1945d59b25d860b8c43a0d945a3d03de3bfa3370
nkuzmit <n.kuzmit@gmail.com>
2016-08-06 17:11:11 +0300

    .

--- src/hw2/ZeroMult.java
+++ src/hw2/ZeroMult.java
@@ -3,7 +3,7 @@ package hw2;
 public class ZeroMult {
 public static void main(String[] args) {
 		
-		int[] f = {1,4,6,8,9,8,5,3,0,7};
+		int[] f = {1,4,6,8,9,0,5,3,0,7};
 		
 		int mult = 1;  //To not get a 0 result we musn't initiate int mult = 0;
 		for(int x = 0; x < f.length & f[x] != 0; x++){	
